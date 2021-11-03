397f36ad5ccfe45d69ad9b9ba05c0285eeda5f87
aqua5201314 <aqua5201314@outlook.com>
2021-11-03 01:03:16 -0700

    编写server

---
+++ src/server/chatserver.cpp
@@ -0,0 +1,20 @@
+#include "chatserver.hpp"
+
+#include <functional>
+using namespace std;
+using namespace placeholders;
+
+ChatServer::ChatServer(EventLoop *loop,
+                       const InetAddress &listenAddr,
+                       const string &nameArg)
+    : _server(loop, listenAddr, nameArg), _loop(loop)
+{
+    // 注册连接回调
+    _server.setConnectionCallback(std::bind(&ChatServer::onConnection, this, _1));
+
+    // 注册消息回调
+    _server.setMessageCallback(std::bind(&ChatServer::onMessage, this, _1, _2, _3));
+
+    // 设置线程数量
+    _server.setThreadNum(4);
+}
