96db7efc077bafbbec8d5b2acbc042c8db1c90c1
aqua5201314 <aqua5201314@outlook.com>
2021-11-03 00:04:11 -0700

    完善文件结构

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -44,7 +44,7 @@ endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
-  include("/root/project/QQ/build/testmuduo/cmake_install.cmake")
+  include("/root/project/QQ/build/test/cmake_install.cmake")
 
 endif()
 
